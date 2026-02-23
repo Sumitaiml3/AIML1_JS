8e744484b90d33abd1d42032fbcc8cbc1256c43f
Sumit Jaiswal <sumitjaiswal@Sumits-MacBook-Air-3.local>
2026-02-23 15:43:17 +0530

    class work 23/02/2026

--- script.js
+++ script.js
@@ -27,8 +27,46 @@ if(sym1===sym2){
 else{
     console.log("false");
 }*/
-var a=100;var b=13;var c=10; 
+/*var a=100;var b=13;var c=10; 
 var linebreak="\n";
 document.write("a+b+c=");
 document.write(a+b+c);
+document.write(linebreak);*/
+/*var a=100;var b=20;
+var linebreak="<br>";
+document.write("a==b)=>");
+result=(a==b);
+document.write(result);
 document.write(linebreak);
+document.write("a!=b)=>");
+result=(a!=b);
+document.write(result);
+document.write(linebreak);
+document.write("a>b)=>");
+result=(a>b);
+document.write(result);
+document.write(linebreak);
+document.write("a<b)=>");
+result=(a<b);
+document.write(result);
+document.write(linebreak);
+document.write("a>=b)=>");
+result=(a>=b);
+document.write(result);
+document.write(linebreak);
+document.write("a<=b)=>");
+result=(a<=b);
+document.write(result);*/
+/*var count;
+document.write("Starting loop: "+"<br>");
+for(count=1;count<=10;count++){
+    document.write("Count is: "+count+"<br>");
+}
+document.write("Loop ended.");*/
+/*for (key in object) {
+    // code block to be executed
+}*/
+const person = {fname:"Ajay", lname:"Singh", age:25};
+for (let x in person) {
+    document.write("person details:" + x + ": " + person[x] + "<br>");
+}

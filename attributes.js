b5a532402d2ddf02b251a794afdb902ae1424adb
Sumit Jaiswal <sumitjaiswal@Sumits-MacBook-Air-3.local>
2026-03-19 23:42:47 +0530

    Add attributes.js file

---
+++ attributes.js
@@ -0,0 +1,11 @@
+const element = document.getElementById("myH1");
+let text = element.getAttribute("class");
+document.getElementById("demo").innerHTML = text;
+
+
+
+//setAttribute() method
+
+function myFunction() {
+    document.getElementById("myH1").setAttribute("class", "democlass");
+}

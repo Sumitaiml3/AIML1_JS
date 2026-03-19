99e724a873af71c218105f6efd061564384f4352
Sumit Jaiswal <sumitjaiswal@Sumits-MacBook-Air-3.local>
2026-03-19 23:44:14 +0530

    Add DOM.js file

---
+++ DOM.js
@@ -0,0 +1,9 @@
+const result = document.getElementById("Para");
+console.log(result);
+result.innerHTML = "my self";
+result.title = "myname";
+
+//Selecting with class
+const result1 = document.getElementsByClassName("heading");
+console.log(result1);
+result1.innerHTML = "hii";
